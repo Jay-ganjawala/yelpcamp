cfbb454808d370902ad5a3dfee5cb36afcab6115
Jay <90839627+Jay-ganjawala@users.noreply.github.com>
2025-07-25 22:53:30 +0530

    Update index.js

--- pages/campgrounds/[id]/index.js
+++ pages/campgrounds/[id]/index.js
@@ -173,9 +173,12 @@ const CampgroundDetail = ({ campground = camp, user = null }) => {
       <section className='flex flex-col gap-4 py-10 lg:gap-6 lg:py-16'>
         <div className='relative'>
           <Image src={planes} className='absolute right-0 z-[-1]' />
-          <h1 className='mb-2 font-volkhov text-3xl text-brand lg:mb-4 lg:text-4xl 2xl:text-5xl'>
-            {campground?.name}
-          </h1>
+          <div className="flex gap-2 items-center mb-2 lg:mb-4">
+            <h1 className='font-volkhov text-3xl text-brand lg:text-4xl 2xl:text-5xl'>
+              {campground?.name}
+            </h1>
+            {campground.plusExclusive && <svg xmlns="http://www.w3.org/2000/svg" title="Plus Exclusive" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-badge-check-icon lucide-badge-check"><path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z"/><path d="m9 12 2 2 4-4"/></svg>}
+          </div>
           <p className='text-sm'>
             {`${campground?.location.city}, ${campground?.location.state}, ${campground?.location.country}`}{' '}
             &middot;{' '}
